7e1b4e8a9b08b2140b33a92da45ceaa5db3738c1
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-11-12 20:17:10 +0000

    Switch to English

--- MATLAB fittings/speed_run.m
+++ MATLAB fittings/speed_run.m
@@ -3,17 +3,17 @@ close
 clc
 
 table = load("speed_run.txt");
-n = table(1:end,1); % Tamanho da estrada
-t = table(1:end,2); % Tempo para determinar a solução
+n = table(1:end,1); % Road size
+t = table(1:end,2); % Time to find a solution
 
-% Plot original dos dados
+% Original data plot
 plot(n,t)
-legend('Curva original')
-title('Tempo em função do tamanho')
-xlabel('Tamanho da estrada')
-ylabel('Tempo para determinar a solução')
+legend('Original curve')
+title('Time as a function of road size')
+xlabel('Road size')
+ylabel('Time to find a solution (s)')
 
-% Aproximação
+% Curve fitting
 hold on
 f = fit(n,t,'exp1');
 plot(f,n,t)

e9f472b5c49d0a31aea41308de325c29274e565f
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-11-13 01:20:11 +0000

    Extend fitted curve on x-axis to [0,800]

--- MATLAB fittings/speed_run.m
+++ MATLAB fittings/speed_run.m
@@ -7,6 +7,7 @@ n = table(1:end,1); % Road size
 t = table(1:end,2); % Time to find a solution
 
 % Original data plot
+figure(1)
 plot(n,t)
 legend('Original curve')
 title('Time as a function of road size')
@@ -17,3 +18,15 @@ ylabel('Time to find a solution (s)')
 hold on
 f = fit(n,t,'exp1');
 plot(f,n,t)
+
+% Extend fitted curve on x-axis to [0,800]
+x_fit = 0:800;
+coefficients = coeffvalues(f);
+y_fit = coefficients(1) * exp(coefficients(2)*x_fit);
+figure(2)
+plot(x_fit,y_fit)
+xlim([0 850])
+title('Time as a function of road size')
+xlabel('Road size')
+ylabel('Time to find a solution (s)')
+fprintf('Time needed to solve the problem, with size 800: %e seconds\n',y_fit(end))

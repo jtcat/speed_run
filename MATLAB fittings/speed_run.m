29913d307679d18dba392d8dc34d382bddf52ce0
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-11-13 01:25:04 +0000

    Add (missing) comments, oops

--- MATLAB fittings/speed_run.m
+++ MATLAB fittings/speed_run.m
@@ -16,17 +16,19 @@ ylabel('Time to find a solution (s)')
 
 % Curve fitting
 hold on
-f = fit(n,t,'exp1');
+f = fit(n,t,'exp1'); % Alternatively, one can launch the Curve Fitting Toolbox
 plot(f,n,t)
 
 % Extend fitted curve on x-axis to [0,800]
 x_fit = 0:800;
-coefficients = coeffvalues(f);
-y_fit = coefficients(1) * exp(coefficients(2)*x_fit);
+coefficients = coeffvalues(f); % Get fitted curve coefficients
+y_fit = coefficients(1) * exp(coefficients(2)*x_fit); % Equation of fitted curve: a*exp(b*x)
 figure(2)
 plot(x_fit,y_fit)
-xlim([0 850])
+xlim([0 850]) % Graph would look empty (due to exponential values) if no x-lim was applied
 title('Time as a function of road size')
 xlabel('Road size')
 ylabel('Time to find a solution (s)')
+
+%% Print the value
 fprintf('Time needed to solve the problem, with size 800: %e seconds\n',y_fit(end))

6e35dd14a9ffff6e00cfa57a311ad5169d399715
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-11-20 13:06:27 +0000

    Update curve fitting graph

--- MATLAB fittings/speed_run.m
+++ MATLAB fittings/speed_run.m
@@ -10,14 +10,14 @@ t = table(:,2); % Time to find a solution
 figure(1)
 plot(n,t)
 legend('Original curve')
-title('Time as a function of road size')
-xlabel('Road size')
-ylabel('Time to find a solution (s)')
 
 % Curve fitting
 hold on
 f = fit(n,t,'exp1'); % Alternatively, one can launch the Curve Fitting Toolbox
 plot(f,n,t)
+title('Time as a function of road size')
+xlabel('Road size')
+ylabel('Time to find a solution (s)')
 
 % Extend fitted curve on x-axis to [0,800]
 x_fit = 0:800;

6a27f7a3c00c79927c672d93318dc2b3b431bd44
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-11-27 11:11:07 +0000

    Update n=800 MATLAB script

--- MATLAB-fittings/speed_run.m
+++ MATLAB-fittings/speed_run.m
@@ -6,18 +6,13 @@ table = load("Path_to_file.txt"); % Change to appropriate file
 n = table(:,1); % Road size
 t = table(:,2); % Time to find a solution
 
-% Original data plot
-figure(1)
-plot(n,t)
-legend('Original curve')
-
 % Curve fitting
-hold on
+figure(1)
 f = fit(n,t,'exp1'); % Alternatively, one can launch the Curve Fitting Toolbox
-plot(f,n,t)
-title('Time as a function of road size')
+plot(f,n,t,'b.')
 xlabel('Road size')
 ylabel('Time to find a solution (s)')
+legend('Valores originais','Reta ajustada','Location','northwest')
 
 % Extend fitted curve on x-axis to [0,800]
 x_fit = 0:800;
@@ -26,7 +21,6 @@ y_fit = coefficients(1) * exp(coefficients(2)*x_fit); % Equation of fitted curve
 figure(2)
 plot(x_fit,y_fit)
 xlim([0 850]) % Graph would look empty (due to exponential values) if no x-lim was applied
-title('Time as a function of road size')
 xlabel('Road size')
 ylabel('Time to find a solution (s)')
 
